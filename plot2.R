d111956920a62180f3bf74d3c4637ffa3db47c44
edos01 <elilemba@gmail.com>
2016-03-27 13:02:38 +0200

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,3 +1,19 @@
+## Loading downloaded and unziped file in my root directory
+file <- "./household_power_consumption.txt"
+
+## Reading and processing the data(.txt file)
+powerdata <- read.table(file, header=TRUE, sep=";")
+powerdata$Date <- as.Date(powerdata$Date, format="%d/%m/%Y")
+df <- powerdata[(powerdata$Date=="2007-02-01") | (powerdata$Date=="2007-02-02"),]
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
+df$Voltage <- as.numeric(as.character(df$Voltage))
+df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
+
+
 ## This plots the "plot2.png" in the Ex_Proj1_Plots folder in root/data folder
 plot2 <- function() {
   plot(df$timestamp,df$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
